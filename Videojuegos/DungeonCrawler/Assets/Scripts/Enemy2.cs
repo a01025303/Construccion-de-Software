162b25036549d894e85a94975ac90a699ae83d8d
Ana Paula Katsuda Zalce <a01025303@itesm.mx>
2022-05-29 18:31:16 -0500

    update videogame

--- Videojuegos/DungeonCrawler/Assets/Scripts/Enemy2.cs
+++ Videojuegos/DungeonCrawler/Assets/Scripts/Enemy2.cs
@@ -1,26 +1,41 @@
+/*
+Code used for an enemy that follows the main character
+
+Ana Paula Katsuda, Mateo Herrera & Gerardo Gutiérrez
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
+    // Define movement speed
     [SerializeField] private float moveSpeed;
-    private SpriteRenderer sp;
-    private Transform playerTrans;
+    // Enemy's sprite renderer
+    private SpriteRenderer sprite;
+    // Transform from main character
+    private Transform mainCharacterTrans;
     // Start is called before the first frame update
     void Start()
     {
-        sp = GetComponent<SpriteRenderer>();
-        playerTrans = GameObject.FindGameObjectWithTag("Main Character").GetComponent<Transform>();
+        // Get enemy's sprite renderer
+        sprite = GetComponent<SpriteRenderer>();
+        // Get main character's transform through tag
+        mainCharacterTrans = GameObject.FindGameObjectWithTag("Main Character").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, playerTrans.position, moveSpeed * 2 * Time.deltaTime);
-        if(playerTrans.position.x >= transform.position.x)
-            sp.flipX = false; 
-        if(playerTrans.position.x < transform.position.x)
-            sp.flipX = true;
+        // Follow main character
+        transform.position = Vector2.MoveTowards(transform.position, mainCharacterTrans.position, moveSpeed * 2 * Time.deltaTime);
+        // if the main character's position in x is greater than enemy's x 
+        if(mainCharacterTrans.position.x >= transform.position.x)
+            // Set sprite to look to right
+            sprite.flipX = false; 
+        // if the main character's position in x is lower than enemy's x 
+        if(mainCharacterTrans.position.x < transform.position.x)
+            // Set sprite to look to left
+            sprite.flipX = true;
     }
 }

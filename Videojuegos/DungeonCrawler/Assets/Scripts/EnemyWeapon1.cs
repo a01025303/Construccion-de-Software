5d2b6126b254210a922aff5364225d8fb5a39103
Ana Paula Katsuda Zalce <a01025303@itesm.mx>
2022-05-29 20:00:54 -0500

    update videogame

--- Videojuegos/DungeonCrawler/Assets/Scripts/EnemyWeapon1.cs
+++ Videojuegos/DungeonCrawler/Assets/Scripts/EnemyWeapon1.cs
@@ -1,59 +1,64 @@
+/*
+Code used to define the behavior of a rotating weapon
+And to define the damage of such weapon
+
+Ana Paula Katsuda, Mateo Herrera & Gerardo Gutiérrez
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyWeapon1 : MonoBehaviour
 {
-    // 
+    // Projectile's rotation speed
     [SerializeField] private float rotateSpeed;
+    // Allow rotation
     public bool isRotating = true;
-
+    // Target (where to move projectile)
     private Transform target;
+    // Movement speed
     [SerializeField] private float moveSpeed;
-    public GameObject destroyEffect, attackEffect;
-
-    private float lifeTimer;
-    [SerializeField] private float maxLife = 2.0f;
-
 
     // Start is called before the first frame update
     void Start()
     {
+        // set target to main character through tag
         target = GameObject.FindGameObjectWithTag("Main Character").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Call selfRotation function
         SelfRotation();
+        // Move projectile towards main character
         transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-
-        lifeTimer += Time.deltaTime;
-        if(lifeTimer >= maxLife)
-        {
-            Instantiate(destroyEffect, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
     }
 
+    // Rotation function
     private void SelfRotation()
     {
-        if(isRotating)//STEP 02
+        // If allowed, change rotation values 
+        if(isRotating)
         {
-            transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);//STEP 01 Weapon Rotation
+            transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
         }
+        // If not allowed, stop rotation
         else
         {
             transform.Rotate(0, 0, 0);
         }
     }
 
+    // Collision with main character
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // If projectile touches main character
         if(other.gameObject.tag == "Main Character")
         {
-            other.GetComponentInChildren<HealthBar>().hp -= 35;
-            //Instantiate(attackEffect, transform.position, Quaternion.identity);
+            // Substract health points from Main Character
+            other.GetComponentInChildren<HealthBar>().hp -= 10;
+            // Destroy projectile
             Destroy(gameObject);
         }
     }
--- Videojuegos/DungeonCrawler/Assets/Scripts/PlayerMove.cs
+++ Videojuegos/DungeonCrawler/Assets/Scripts/PlayerMove.cs
@@ -1,42 +1,51 @@
+/*
+Code used to make player move while including animation settings
+
+Ana Paula Katsuda, Mateo Herrera & Gerardo Gutiérrez
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Rigidbody2D rigidB2D; 
+    // Camera
     public Camera theCam; 
+    // Movement vector
     Vector3 movement; 
+    // Animate main character with animator
     public Animator animator; 
 
-    //public bool useControl; 
     void Start()
     {
+        // Set camera to Main camera
         theCam = Camera.main;
-        rigidB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // movement getting horizontal and vertical axis 
         movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
+        // Setting animator values with movement
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Magnitude", movement.magnitude);
 
+        // Change position 
         transform.position = transform.position + movement * Time.deltaTime;
 
-        //rigidB2D.velocity = movement; 
-
-
+        // Get mouseposition
         Vector3 mouse = Input.mousePosition; 
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition); 
-        Debug.Log(movement);
+        // Transform position
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition); 
     }
 
+    // Function to freeze movement
     void freeze()
     {
+        // Set everything to 0
         animator.SetFloat("Horizontal", 0.0f);
         animator.SetFloat("Vertical", 0.0f);
         animator.SetFloat("Magnitude", 0.0f);

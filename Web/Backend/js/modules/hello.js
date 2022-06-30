e3d04dcc7305059c384416910968cc1dd3448084
Ana Paula Katsuda <70499251+a01025303@users.noreply.github.com>
2022-06-30 14:24:14 -0500

    Delete Web directory

--- Web/Backend/js/modules/hello.js
+++
@@ -1,8 +0,0 @@
-import { Student } from "./student.js"; 
-// cuando exportamos con export default, no son necesarias las {}
-
-//const {Student} = require("./student.js")
-
-//const st = require(Student)
-const student_1 = new Student("Akemi", "Katsuda", "akatsuda@outlook.com")
-student_1.printInfo()
--- Web/Backend/js/users_test.js
+++
@@ -1,7 +0,0 @@
-import users from '../js/users_module.js'  // Para import, poner ruta absoluta/relativa
-
-const user1 = new users('Akemi', 'Katsuda', 'a01025303@tec.mx')
-
-user1.printName()
-
-document.getElementById("displayInfo").innerHTML = user1.getInfo()
